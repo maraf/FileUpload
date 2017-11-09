c24953b74bbf8275c7d8b4e3ec6fa0d2c272f7d7
Marek Fišera <mara@neptuo.com>
2017-11-09 18:30:30 +0100

    Clean up template.

--- FileUpload.Web.UI/Controllers/HomeController.cs
+++ FileUpload.Web.UI/Controllers/HomeController.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿using FileUpload.Web.UI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using FileUpload.Web.UI.Models;
 
 namespace FileUpload.Web.UI.Controllers
 {
@@ -14,21 +14,7 @@ namespace FileUpload.Web.UI.Controllers
         {
             return View();
         }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
+        
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
