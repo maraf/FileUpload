7a47cd78fc89e4b80f3fa9c9bc1f1d65cbca7e94
Marek Fišera <mara@neptuo.com>
2017-11-09 20:33:15 +0100

    Upload status progress.

--- src/FileUpload.Web.UI/wwwroot/js/site.js
+++ src/FileUpload.Web.UI/wwwroot/js/site.js
@@ -1,1 +1,36 @@
 ﻿// Write your JavaScript code.
+
+function UploadFile(file, onCompleted, onError, onProgress) {
+    var formData = new FormData();
+    formData.append("file", file, file.name);
+
+    var currentRequest = new XMLHttpRequest();
+    currentRequest.onreadystatechange = function (e) {
+        var request = e.target;
+
+        if (request.readyState == XMLHttpRequest.DONE) {
+            if (request.status == 200) {
+                var responseText = currentRequest.responseText;
+                onCompleted(responseText);
+            }
+            else if (onError != null) {
+                onError(currentRequest.status, currentRequest.statusText);
+            }
+        }
+    };
+
+    if (onError != null) {
+        currentRequest.onerror = function (e) {
+            onError(500, e.message)
+        };
+    }
+
+    if (onProgress != null) {
+        currentRequest.upload.onprogress = function (e) {
+            onProgress(e.loaded, e.total);
+        };
+    }
+
+    currentRequest.open("POST", Url);
+    currentRequest.send(formData);
+}
