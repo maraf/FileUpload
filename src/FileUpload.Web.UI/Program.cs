f2646192978cf70c3e83675383a7e573eb535a4b
Marek Fišera <mara@neptuo.com>
2017-11-09 22:03:29 +0100

    Change port.

--- src/FileUpload.Web.UI/Program.cs
+++ src/FileUpload.Web.UI/Program.cs
@@ -19,6 +19,7 @@ namespace FileUpload.Web.UI
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://0.0.0.0:5001")
                 .UseStartup<Startup>()
                 .Build();
     }
