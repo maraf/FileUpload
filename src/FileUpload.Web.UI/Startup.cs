5866474b8da4201661b2892c956f2541b40c6406
Marek Fišera <mara@neptuo.com>
2018-04-25 20:32:37 +0200

    Reformat code.

--- src/FileUpload.Web.UI/Startup.cs
+++ src/FileUpload.Web.UI/Startup.cs
@@ -40,7 +40,8 @@ namespace FileUpload.Web.UI
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=Index}/{id?}"
+                );
             });
         }
     }
